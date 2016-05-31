dcedb2d20b0cc0479621952826af97f4e02f85ab
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-05-31 20:12:09 +0100

    spec tests working

---
+++ test/e2e/app.spec.js
@@ -0,0 +1,10 @@
+describe("app", function() {
+  it("should get home page title", function() {
+    browser.get('/');
+    expect(browser.getTitle()).toEqual("Todos App");
+  });
+  it("should say 'Hello world' on the page", function(){
+    browser.get('/')
+    expect($$("p").first().getText()).toEqual("Hello World");
+  });
+});
