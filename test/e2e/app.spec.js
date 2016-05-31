2ff9846081f0b4dd3a4b39eaade89648dca7e630
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-05-31 20:16:18 +0100

    first test for actual app

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -1,10 +1,7 @@
-describe("app", function() {
-  it("should get home page title", function() {
+describe("Todos tracker", function() {
+  it("has a todo", function(){
     browser.get('/');
-    expect(browser.getTitle()).toEqual("Todos App");
-  });
-  it("should say 'Hello world' on the page", function(){
-    browser.get('/')
-    expect($$("p").first().getText()).toEqual("Hello World");
+    var todo = $('#todo');
+    expect(todo.getText()).toEqual("Todo1");
   });
 });
